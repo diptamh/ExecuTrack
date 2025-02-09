ff9f5cf32ad5223cba83fd4e3822eced06f8b674
diptamh <halderdiptam@gmail.com>
2025-02-09 08:11:06 +0530

    Added loader

--- src/client/components/objectSelector/index.js
+++ src/client/components/objectSelector/index.js
@@ -1,64 +1,97 @@
 import React, { Component } from "react";
-import { MenuItem, Select, InputLabel, FormControl } from "@mui/material";
+import {
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  CircularProgress,
+} from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import APIService from "../../service/APIService.js";
 
 const theme = createTheme();
 
+const styles = {
+  formControl: {
+    color: "black",
+    backgroundColor:
+      theme.palette.mode === "light"
+        ? theme.palette.grey[200]
+        : theme.palette.grey[700],
+    borderRadius: 2,
+    width: "80%",
+    marginLeft: "10%",
+  },
+  loadingContainer: {
+    display: "flex",
+    alignItems: "center",
+    padding: "8px 14px",
+  },
+};
+
 class ObjectSelector extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      selectedObject: "",
-    };
-  }
+  state = {
+    selectedObject: "",
+    objects: [],
+    isLoading: false,
+    isObjectsLoaded: false,
+  };
+
+  handleClick = async () => {
+    // Only fetch objects if they haven't been loaded yet
+    if (!this.state.isObjectsLoaded && !this.state.isLoading) {
+      this.setState({ isLoading: true });
+      try {
+        const response = await APIService.getObjects();
+        this.setState({
+          objects: response.data,
+          isLoading: false,
+          isObjectsLoaded: true,
+        });
+      } catch (error) {
+        console.error("Failed to fetch objects:", error);
+        this.setState({ isLoading: false });
+      }
+    }
+  };
 
   handleSelect = (event) => {
     const selected = event.target.value;
     this.setState({ selectedObject: selected });
-
-    // this is used to pass the selected object to the parent component
     this.props.OnObjectSelection(selected);
   };
 
-  componentDidMount() {
-    // this is used to get the objects from the salesforce
-    APIService.getObjects().then((response) => {
-      console.log("objects->", response.data);
-      this.setState({ objects: response.data });
-    });
-  }
-
   render() {
+    const { selectedObject, objects, isLoading } = this.state;
+
     return (
-      <FormControl
-        fullWidth
-        sx={{
-          color: "black",
-          backgroundColor: (theme) =>
-            theme.palette.mode === "light"
-              ? theme.palette.grey[200]
-              : theme.palette.grey[700],
-          borderRadius: 2,
-          width: "80%",
-          marginLeft: "10%",
-        }}
-      >
+      <FormControl fullWidth sx={styles.formControl}>
         <InputLabel id="object-selector-label" sx={{ color: "Black" }}>
           Object
         </InputLabel>
         <Select
           labelId="object-selector-label"
           id="object-selector"
-          value={this.state.selectedObject}
+          value={selectedObject}
           label="Object"
           onChange={this.handleSelect}
+          onOpen={this.handleClick}
+          disabled={isLoading}
         >
-          {this.state.objects?.map((object) => (
-            <MenuItem value={object.name} key={object.name}>
-              {object.name}
+          {isLoading ? (
+            <MenuItem disabled>
+              <div style={styles.loadingContainer}>
+                <CircularProgress size={20} />
+                <span style={{ marginLeft: 10 }}>Loading objects...</span>
+              </div>
             </MenuItem>
-          ))}
+          ) : (
+            objects?.map((object) => (
+              <MenuItem value={object.name} key={object.name}>
+                {object.name}
+              </MenuItem>
+            ))
+          )}
         </Select>
       </FormControl>
     );
